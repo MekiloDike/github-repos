a1950ebff1d3f42c31aa0aec60c2f7d41cad3254
Ugwuja chukwuemeka <156838239+MekiloDike@users.noreply.github.com>
2024-05-12 21:38:46 +0100

    github Api consuming
    git Api consuming

---
+++ github-repos-main/github-repos-main/git-repos/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import GitHubRepos from './components/GitHubRepos'
+
+const App = () => {
+  return (
+    <>
+      <GitHubRepos/>
+    </>
+  )
+}
+
+export default App
